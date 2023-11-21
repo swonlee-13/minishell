a7fd289e1019873a64b75384bc3acf57b0ba1022
이연재 <leeyj0304@gmail.com>
2023-11-22 02:19:58 +0900

    refactor: Remove count_len(char **strs) function for enhanced functionality

--- srcs/libft/ft_free.c
+++ srcs/libft/ft_free.c
@@ -3,34 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_free.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seongwol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: yeolee2 <yeolee2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 20:36:18 by seongwol          #+#    #+#             */
-/*   Updated: 2023/08/03 20:37:37 by seongwol         ###   ########.fr       */
+/*   Updated: 2023/11/22 02:17:36 by yeolee2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_len(char **strs)
-{
-	int	i;
+// static int	count_len(char **strs)
+// {
+// 	int	i;
 
-	i = 0;
-	if(strs)
-		while (strs[i] != NULL)
-			i++;
-	return (i);
-}
+// 	i = 0;
+// 	if(strs)
+// 		while (strs[i] != NULL)
+// 			i++;
+// 	return (i);
+// }
 
 void	ft_free(char **strs)
 {
 	int	i;
-	int	len;
 
 	i = 0;
-	len = count_len(strs);
-	while (i < len)
+	while (strs[i])
 	{
 		free(strs[i]);
 		i++;
