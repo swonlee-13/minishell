6d071f70c99bae4750af61da9e99658a54ae557f
이연재 <leeyj0304@gmail.com>
2023-11-22 03:48:13 +0900

    feat: Add initial minishell.h header file with basic structure

---
+++ includes/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeolee2 <yeolee2@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 03:09:36 by yeolee2           #+#    #+#             */
+/*   Updated: 2023/11/22 03:43:18 by yeolee2          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "../libft/libft.h"
+# include "parse.h"
+# define TRUE 1
+# define FALSE 0
+# define SYNTAX_ERROR -1
+
+#endif
