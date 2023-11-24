2f9cf0897e67cd831f5ee26b8e3e1676277181eb
이연재 <leeyj0304@gmail.com>
2023-11-24 18:42:26 +0900

    test: Add test code for chdir function

--- test.c
+++ test.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <unistd.h>
 
+# define PATH_MAX	1024
+
 char	*find_env(char **env, char *str)
 {
 	int	i;
@@ -18,25 +20,70 @@ char	*find_env(char **env, char *str)
 	return (env[i]);
 }
 
-int main(int ac, char **av, char **env)
-{
-	(void)ac; (void)av;
-	char	*tmp = malloc(sizeof(char) * 1024);
-	char	*str;
+// int main(int ac, char **av, char **env)
+// {
+// 	(void)ac; (void)av;
+// 	char	*tmp = malloc(sizeof(char) * PATH_MAX);
+// 	char	*str;
 
-	getcwd(tmp, 1024);
-	printf("current directory : %s\n", tmp);
-	str = find_env(env, "PWD=");
-	printf("%s\n", str);
-	str = find_env(env, "OLDPWD=");
-	printf("%s\n", str);
-	printf("\n\n-----------------------------\n\n\n");
-	chdir("..");
-	getcwd(tmp, 1024);
-	printf("after chdir : %s\n", tmp);
-	str = find_env(env, "PWD=");
-	printf("%s\n", str);
-	str = find_env(env, "OLDPWD=");
-	printf("%s\n", str);
-	free(tmp);
+// 	getcwd(tmp, PATH_MAX);
+// 	printf("current directory : %s\n", tmp);
+// 	str = find_env(env, "PWD=");
+// 	printf("%s\n", str);
+// 	str = find_env(env, "OLDPWD=");
+// 	printf("%s\n", str);
+// 	printf("\n\n-----------------------------\n\n\n");
+// 	chdir("..");
+// 	getcwd(tmp, PATH_MAX);
+// 	printf("after chdir : %s\n", tmp);
+// 	str = find_env(env, "PWD=");
+// 	printf("%s\n", str);
+// 	str = find_env(env, "OLDPWD=");
+// 	printf("%s\n", str);
+// 	free(tmp);
+// }
+
+int main(void)
+{
+    int ret;
+    char	*tmp = malloc(sizeof(char) * PATH_MAX);
+
+    tmp = getcwd(tmp, PATH_MAX);
+    printf("current PATH: %s\n", tmp);
+	printf("------------------------------------\n");
+    tmp = getenv("HOME");
+	printf("getenv(\"HOME\") = %s\n", tmp);
+	ret = chdir(tmp);
+	printf("%s\n", tmp);
+    printf("ret: %d\n", ret);
+	printf("------------------------------------\n");
+    tmp = "Desktop";
+	ret = chdir(tmp);
+	printf("%s\n", tmp);
+    printf("ret: %d\n", ret);
+	printf("------------------------------------\n");
+    tmp = "/42Seoul";
+	ret = chdir(tmp);
+	printf("%s\n", tmp);
+    printf("ret: %d\n", ret);
+	printf("------------------------------------\n");
+    tmp = "42Seoul";
+	ret = chdir(tmp);
+	printf("%s\n", tmp);
+    printf("ret: %d\n", ret);
+	printf("------------------------------------\n");
+	tmp = "-";
+	ret = chdir(tmp);
+	printf("%s\n", tmp);
+    printf("ret: %d\n", ret);
+	printf("------------------------------------\n");
+	tmp = ".";
+	ret = chdir(tmp);
+	printf("%s\n", tmp);
+    printf("ret: %d\n", ret);
+	printf("------------------------------------\n");
+	tmp = "..";
+	ret = chdir(tmp);
+	printf("%s\n", tmp);
+    printf("ret: %d\n", ret);
 }
