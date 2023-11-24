2feb0b9a94a480c066a6c00e1c00607796bab2cc
이연재 <leeyj0304@gmail.com>
2023-11-25 00:58:38 +0900

    feat: Enhance cd command and add basic test framework setup

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,22 +6,107 @@
 /*   By: yeolee2 <yeolee2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 01:05:18 by yeolee2           #+#    #+#             */
-/*   Updated: 2023/11/22 03:50:40 by yeolee2          ###   ########.fr       */
+/*   Updated: 2023/11/25 00:56:18 by yeolee2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "../../includes/minishell.h"
+#include <string.h>
 
-void    change_directory(char **vector)
+char    *get_env(char **env, char *str)
 {
-    char    *target;
-    if (ft_strslen(vector) < 1)
+    int     idx;
+    char    *res;
+
+    idx = 0;
+    while (env[idx])
     {
-        //TODO: Adjusments of the errno be made
-        perror("cd: Missing argument");
-        return ;
+        //TODO: Is it okay to use strncmp when searching for the env variable?
+        if (!ft_strncmp(str, env[idx], ft_strlen(str)) && env[idx][ft_strlen(str) + 1] == '=')
+            break;
+        idx++;
     }
-    target = vector[1];
-    if (chdir(target) != 0)
-        perror("cd: Failed to change directory");
+    return (env[idx]);
+}
+
+void    change_directory(char **vector, char **env)
+{
+    char    *curr_directory;
+    char    *prev_directory;
+    char    *targ_directory;
+
+    //TODO: prev_directory should check OLDPWD
+    if (find_env(env, "OLDPWD"))
+        prev_directory = NULL;
+    targ_directory = vector[1];
+    //TODO: How to initialize curr_directory
+    if (getcwd(curr_directory, PATH_MAX))
+        printf("%s\n", curr_directory);
+        // curr_directory = NULL;
+    if (!vector[1])
+        targ_directory = getenv("HOME");
+    else if (!ft_strcmp(vector[1], "-"))
+    {
+        // If OLDPWD not set, print error message.
+        if (!prev_directory)
+            printf("minishell: cd: OLDPWD not set\n");
+        else
+        {
+            printf("%s\n", prev_directory);
+            targ_directory = prev_directory;
+        }
+    }
+    //TODO: SUCCESS MACRO
+    if (!chdir(targ_directory))
+    {
+        prev_directory = curr_directory;
+        getcwd(curr_directory, PATH_MAX);
+    }
+    else
+        printf("minishell: cd: %s: No such file or directory\n", vector[1]);
+    //TODO: Modify PWD and OLDPWD with curr_directory and prev_directory
+}
+
+char** build_vector(const char* arg)
+{
+    char** vector = malloc(3 * sizeof(char *));
+    vector[0] = strdup("cd");  // First argument is usually the command name
+    vector[1] = strdup(arg);
+    vector[2] = NULL;  // Argument list must be NULL terminated
+    return vector;
+}
+
+void free_vector(char** vector) {
+    free(vector[0]);
+    free(vector[1]);
+    free(vector);
+}
+
+int main(void)
+{
+    char    *dir;
+    char    **env;
+    char    **vector;
+
+    printf("-----------------------------------------\n");
+    vector = build_vector("/Users/iyeonjae/Desktop/minishell/srcs");
+    change_directory(vector, env);
+    free_vector(vector);
+    printf("dir: %s\n", getcwd(dir, PATH_MAX));
+    printf("-----------------------------------------\n");
+    vector = build_vector("builtins");
+    change_directory(vector, env);
+    free_vector(vector);
+    printf("dir: %s\n", getcwd(dir, PATH_MAX));
+    printf("-----------------------------------------\n");
+    vector = build_vector("-");
+    change_directory(vector, env);
+    free_vector(vector);
+    printf("dir: %s\n", getcwd(dir, PATH_MAX));
+    printf("-----------------------------------------\n");
+    vector = build_vector("/");
+    change_directory(vector, env);
+    free_vector(vector);
+    printf("dir: %s\n", getcwd(dir, PATH_MAX));
+    printf("-----------------------------------------\n");
 }
