ec1189fcf4957addfc8bb18bc3b804c88d8380ae
이연재 <leeyj0304@gmail.com>
2023-12-02 01:16:39 +0900

    fix: Error handling when getcwd returns NULL

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: yeolee2 <yeolee2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 02:51:10 by yeolee2           #+#    #+#             */
-/*   Updated: 2023/12/01 23:08:47 by yeolee2          ###   ########.fr       */
+/*   Updated: 2023/12/02 01:14:39 by yeolee2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void    print_working_directory(void)
 
     pwd = getcwd(NULL, 0);
     //TODO: getcwd error handling
-    // if (pwd == NULL)
+    if (pwd == NULL)
+        printf("%s\n", strerror(errno));
     printf("%s\n", pwd);
 }
