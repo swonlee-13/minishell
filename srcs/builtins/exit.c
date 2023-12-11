97cc4287cf222237113a3a2aff26dd1c3e3e0b48
이연재 <leeyj0304@gmail.com>
2023-12-12 02:48:03 +0900

    feat: Implement Exit Command Logic with Argument Validation
    This commit enhances the functionality of the 'exit' builtin command in the minishell project:
    
    1. **Argument Validation**: Implemented 'check_exit_arg' to validate the numeric nature of the argument provided to the 'exit' command. If a non-numeric argument is detected, the shell exits with a status code of 255 and prints an appropriate error message.
    
    2. **Exit Status Handling**: In 'terminate_program', the shell now correctly processes the argument for the 'exit' command. If no argument is provided, the shell exits with 'EXIT_SUCCESS'. For a single argument, it attempts to convert it to an exit status, handling non-numeric arguments as errors.
    
    3. **Global Exit Code**: Utilized 'g_exit_code' globally to manage the exit status consistently across the shell.
    
    These updates ensure more robust and standard-compliant handling of the 'exit' command, improving the shell's reliability and user feedback in case of errors.

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,9 +6,47 @@
 /*   By: yeolee2 <yeolee2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 10:10:20 by yeolee2           #+#    #+#             */
-/*   Updated: 2023/11/27 11:00:23 by yeolee2          ###   ########.fr       */
+/*   Updated: 2023/12/12 00:45:25 by yeolee2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/minishell.h"
+#include "minishell.h"
 
+extern int g_exit_code;
+
+int    check_exit_arg(char **vector)
+{
+    int     idx;
+    char    *arg;
+    
+    arg = vector[0];
+    idx = 0;
+    while (arg[idx])
+    {
+        if (!ft_isdigit(arg[idx]))
+        {
+            g_exit_code = 255;
+            print_error_message(vector, "numeric argument required\n");
+            exit(g_exit_code);
+        }
+    }
+    return (SUCCESS);
+}
+
+void    terminate_program(char **vector)
+{
+    if (vector[1] == NULL)
+        exit(EXIT_SUCCESS);
+    if (ft_strslen(vector) == 2)
+    {
+        g_exit_code = ft_atoi(vector[1]);
+        if (g_exit_code == 0)
+        {
+            if (check_exit_arg(vector) == SUCCESS)
+                exit(g_exit_code);
+        }
+        else
+            exit(g_exit_code);
+    }
+    g_exit_code = 1;
+}
