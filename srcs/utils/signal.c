9aec3d1a372a1b1e7e75e18668b784a82a01ca02
이연재 <leeyj0304@gmail.com>
2023-12-13 20:13:13 +0900

    fix: Bug fix regarding the test case "cat | cat | ls" where redir->temp not being closed was the problem

--- srcs/utils/signal.c
+++ srcs/utils/signal.c
@@ -6,11 +6,13 @@
 /*   By: yeolee2 <yeolee2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 22:35:19 by yeolee2           #+#    #+#             */
-/*   Updated: 2023/12/13 01:28:36 by yeolee2          ###   ########.fr       */
+/*   Updated: 2023/12/13 17:47:56 by yeolee2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+extern int g_exit_code;
 
 void    prompt_sigint_handler(int signum)
 {
@@ -21,13 +23,18 @@ void    prompt_sigint_handler(int signum)
     rl_redisplay();
 }
 
+void    child_sigint_handler(void)
+{
+    // signal(SIGINT, SIG_DFL);
+    // printf("\n");
+}
+
 void    heredoc_sigint_handler(int signum)
 {
     (void)signum;
+    close(STDIN_FILENO);
+    g_exit_code = 1;
     printf("\n");
-    rl_replace_line("", 1);
-    rl_on_new_line();
-    rl_redisplay();
 }
 
 void    init_signal()
